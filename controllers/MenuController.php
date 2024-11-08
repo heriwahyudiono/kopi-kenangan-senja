51df6a728bd74b1de8d53930bf2d662618b944e0
heriwahyudiono <heriwhydiono@gmail.com>
2024-11-08 08:15:53 +0700

    feat: first commit

--- controllers/MenuController.php
+++ controllers/MenuController.php
@@ -1,18 +1,14 @@
 <?php
-// Memanggil model menu
-require_once '../models/MenuModel.php';
+require_once 'models/MenuModel.php';
 
 class MenuController {
     private $model;
 
     public function __construct() {
-        // Membuat objek baru dari kelas MenuModel
-        $model = new MenuModel();
+        $this->model = new MenuModel();
     }
 
-    // Fungsi untuk mengambil menu dari MenuModel
     public function getMenus() {
-        // Mengembalikan hasil menu dari fungsi getMenus
         return $this->model->getMenus();
     }
 }
